0f5b4a11b5752403732342a3b000be2d87ff58fb
Jason Back <jason.back01@gmail.com>
2023-08-03 14:01:38 -0400

    created functions using filter()

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function findMatching(array, string) {
+    return array.filter((el) => el.toLowerCase().includes(string.toLowerCase()))
+}
+
+function fuzzyMatch(array, query) {
+    return array.filter((el) => el.toLowerCase().startsWith(query.toLowerCase()))
+}
+
+function matchName(array, string){
+    return array.filter((obj) => obj.name === string)
+}
+console.log(matchName(drivers, 'Bobby'))
